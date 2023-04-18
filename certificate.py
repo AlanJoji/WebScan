6368573b340fac7dceca96c4e428821f5ea5d6c6
AlanJoji <alanjojiveliyath@yahoo.com>
2023-04-18 22:10:32 +0530

    Implemented SSL checking - requests + ssl

--- certificate.py
+++ certificate.py
@@ -1,23 +1,68 @@
 from socket import *
 from ssl import *
+from requests import *
 
-def get_certificate (hostname) :
+
+# Functions
+def connection_request (url) :
+    result = False
+    try :
+        response = get(url)
+        print(response)
+
+        result = True
+        return result
+
+    except error :
+        print(error)
+        return result
+
+
+def url_is_safe (url) :
+    result = connection_request(url)
+    return result
+
+
+def hostname_is_safe (hostname) :
     context = create_default_context()
 
-    unsecure_socket = create_connection((hostname, 443))
-    secure_socket = context.wrap_socket(unsecure_socket, server_hostname=hostname)
+    unsecure_socket = create_connection((hostname, 443))    
+    result = verify_certificate(context, unsecure_socket)
 
-    certificate = secure_socket.getpeercert()
-    # print(certificate)
+    return result
 
-    secure_socket.close()
-    unsecure_socket.close()
+def verify_certificate (context, unsecure_socket) :
+    result = False
+    try :
+        secure_socket = context.wrap_socket(unsecure_socket, server_hostname = hostname)
 
-# Todo: Implement the certificate verification part using requests as well as ssl
-def verify_certificate () :
-    return None
+        certificate = secure_socket.getpeercert()
 
+        for element in certificate :
+            print(element.upper(), ": ", certificate[element])
+            print()
+
+        secure_socket.close()
+        unsecure_socket.close()
+
+        result = True
+        return True
+
+    except SSLCertVerificationError :
+        print(error)
+
+        return result
+
+# Main
 if __name__ == "__main__" :
-    hostname = input("Enter the host name :")
-    get_certificate(hostname)
+    hostname = "www.facebook.com"
+    hostname_result =  hostname_is_safe(hostname)
+    print(hostname_result)
+    
+    print()
+
+    url = "https://alanjoji.com"
+    url_result = url_is_safe(url)
+    print(url_result)
+
 
