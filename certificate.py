9c97529c5096ba0584aa098772ebce8bfc2a0fc3
AlanJoji <alanjojiveliyath@yahoo.com>
2023-04-19 19:32:03 +0530

    Added documentation - SSL

--- certificate.py
+++ certificate.py
@@ -5,9 +5,18 @@ from requests import *
 
 # Functions
 def check_url (url) :
+    """
+    Input:  
+        URL of a website 
+    Action: 
+        Generates a request to the webpage with the get method with the verify set to True
+    Output:     
+        True for Site with SSL Certificate
+        False for Site without SSL Certifcate
+    """
     result = False
     try :
-        response = get(url)
+        response = get(url, verify = True)
         print(response)
 
         result = True
@@ -19,11 +28,30 @@ def check_url (url) :
 
 
 def url_is_safe (url) :
+    """
+    Input:  
+        URL of a website 
+    Action: 
+        Parent function to check_url
+    Output:     
+        True for Site with SSL Certificate
+        False for Site without SSL Certifcate
+    """
     result = check_url(url)
     return result
 
 
 def check_hostname (context, unsecure_socket) :
+    """
+    Input:  
+        Context for SSL set up using the HTTPS port (443)
+        Unsecure Socket which has already been connected to the server
+    Action: 
+        Wraps a SSL context to the existing unsecure port to check whether the site is secure
+    Output:     
+        True for Site with SSL Certificate -> Can also produce the SSL Certificate 
+        False for Site without SSL Certifcate -> Error is handled when the SSL wrap fails
+    """
     result = False
     try :
         secure_socket = context.wrap_socket(unsecure_socket, server_hostname = hostname)
@@ -46,6 +74,15 @@ def check_hostname (context, unsecure_socket) :
         return result
 
 def hostname_is_safe (hostname) :
+    """
+    Input:  
+        Hostname of the website
+    Action: 
+        Creates a context and a unsecure connection
+    Output:     
+        True for Site with SSL Certificate 
+        False for Site without SSL Certifcate 
+    """
     context = create_default_context()
 
     unsecure_socket = create_connection((hostname, 443))    
@@ -53,8 +90,7 @@ def hostname_is_safe (hostname) :
 
     return result
 
-# Main
-if __name__ == "__main__" :
+def tester () :
     hostname = "www.facebook.com"
     hostname_result =  hostname_is_safe(hostname)
     print(hostname_result)
@@ -65,4 +101,8 @@ if __name__ == "__main__" :
     url_result = url_is_safe(url)
     print(url_result)
 
+    
+# Main
+if __name__ == "__main__" :
+    tester()
 
